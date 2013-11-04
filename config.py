cb53cfa1e9a5bd75069b8d27d6eb5d6084dcb613
Mitchell Stewart <personal.mitchellstewart@gmail.com>
2013-11-05 05:16:48 +1100

    Added config and users for production

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 BASE_URI = 'https://graph.facebook.com/'
-FB_APP_ID = ''
-FB_APP_SECRET = ''
-HOUR = 6
+FB_APP_ID = '498777246878058'
+FB_APP_SECRET = '02272a1ef565d2bbbec38c64e464094f'
+HOUR = 11
+

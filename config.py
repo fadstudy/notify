9e038b806da5afb2311b8fd80112f7a12dfe446d
Mitchell Stewart <personal.mitchellstewart@gmail.com>
2013-11-05 15:23:54 +1100

    Added variable in config

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+BASE_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
 BASE_URI = 'https://graph.facebook.com/'
 FB_APP_ID = '498777246878058'
 FB_APP_SECRET = '02272a1ef565d2bbbec38c64e464094f'

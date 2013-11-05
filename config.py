bda223a802e3333ad6f41473f854d9c4bf3af89e
Mitchell Stewart <personal.mitchellstewart@gmail.com>
2013-11-06 04:31:00 +1100

    Changed config to an hour earlier

--- config.py
+++ config.py
@@ -4,4 +4,5 @@ BASE_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
 BASE_URI = 'https://graph.facebook.com/'
 FB_APP_ID = '498777246878058'
 FB_APP_SECRET = '02272a1ef565d2bbbec38c64e464094f'
-HOUR = 13
+HOUR = 12
+

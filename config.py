502d0dd0bfd3713588392845ea1c27655cdacaed
mylsb <personal.mitchellstewart@gmail.com>
2014-03-17 05:35:50 +0000

    Moved keys and passwords to env vars

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ BASE_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
 
 API_VERSION = 'v0.3'
 API_BASE_URI = 'https://thefadstudy.herokuapp.com/api/{0}/'.format(API_VERSION)
-API_USERNAME = 'apiuser'
-API_PASSWORD = 'letmeinbrah!'
+API_USERNAME = os.environ.get('API_USERNAME')
+API_PASSWORD = os.environ.get('API_PASSWORD')
 
 FB_BASE_URI = 'https://graph.facebook.com/'
-FB_APP_ID = '498777246878058'
-FB_APP_SECRET = '02272a1ef565d2bbbec38c64e464094f'
+FB_APP_ID = os.environ.get('FB_APP_ID')
+FB_APP_SECRET = os.environ.get('FB_APP_SECRET')
 
 HOUR = 14 # The hour of the day to send the notifcation

f5c4a272831d4329556b9cd1958898b7e513a4f8
aussiechris <s3080693@student.rmit.edu.au>
2014-08-27 13:11:06 +1000

    Updated Environment Variable Names in Config.py

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ API_BASE_URI = 'https://thefadstudy.herokuapp.com/api/{0}/'.format(API_VERSION)
 API_USERNAME = environ.get('API_USERNAME')
 API_PASSWORD = environ.get('API_PASSWORD')
 
-FB_BASE_URI = 'https://graph.facebook.com/'
-FB_APP_ID = environ.get('FB_APP_ID')
-FB_APP_SECRET = environ.get('FB_APP_SECRET')
+FACEBOOK_BASE_URI = 'https://graph.facebook.com/'
+FACEBOOK_APP_ID = environ.get('FACEBOOK_APP_ID')
+FACEBOOK_APP_SECRET = environ.get('FACEBOOK_APP_SECRET')
 
 HOUR = environ.get('NOTIFY_HOUR') # The hour of the day to send the notifcation
